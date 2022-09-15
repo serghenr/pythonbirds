7eeaff87abe6ddf571f9ff23e11279e6786bab52
Sérgio Henrique <sergio.henrique.farm@gmail.com>
2022-09-15 18:28:42 -0300

    Criados atributos de instância e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome = None):
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'SH'
+    print(p.nome)
+    print(p.idade)
