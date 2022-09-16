4a18a1dc84df05d3cb7046568ba12753511936fa
Sérgio Henrique <sergio.henrique.farm@gmail.com>
2022-09-16 12:36:09 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,8 @@
 class Pessoa:
+    olhos = 2  #atributo de classe(pois pode ser acessado pela classe) ou atributo default (do inglês = parâmetro padrão)
+
     def __init__(self, *filhos, nome = None, idade = 35):
+        self.idade = idade
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
@@ -19,5 +22,12 @@ if __name__ == '__main__':
         print(filho.nome)
     luciano.sobrenome = 'Ramalho'
     del luciano.filhos
-    print(luciano.__dict__)
+    luciano.olhos = 1
+    del luciano.olhos
     print(renzo.__dict__)
+    print(luciano.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
